785e9668ac6482f7a29580404186a929b3ea7682
Phosu Parsons <38191182+phossil@users.noreply.github.com>
2022-12-23 04:47:53 -0500

    flake.nix: add nixosModules

--- flake.nix
+++ flake.nix
@@ -6,7 +6,10 @@
   };
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
-      let pkgs = import nixpkgs { inherit system; }; in
+      let
+        pkgs = import nixpkgs { inherit system; };
+        nixosModules.default = import ./modules.nix;
+      in
       rec {
         packages = flake-utils.lib.flattenTree {
           qvwm = pkgs.callPackage ./pkgs;
@@ -32,5 +35,5 @@
             imlib
           ];
         });
-      });
+      } // { inherit nixosModules; });
 }
