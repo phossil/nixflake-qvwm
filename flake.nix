7d648240d1f2d6177459cffe2b182bd9b505d87c
Phosu Parsons <38191182+phossil@users.noreply.github.com>
2022-12-23 06:14:30 -0500

    revert flake.nix, move files

--- flake.nix
+++ flake.nix
@@ -6,16 +6,7 @@
   };
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            (self: super: {
-              config.services.xserver.windowManager.qvwm = import ./modules.nix;
-            })
-          ];
-        };
-        #nixosModules.default = import ./modules.nix;
+      let pkgs = import nixpkgs { inherit system; };
       in
       rec {
         packages = flake-utils.lib.flattenTree {
