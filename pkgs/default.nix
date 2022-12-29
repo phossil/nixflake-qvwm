63212ef63aaf8c2f05d41c847564a1b025ae7175
Phosu Parsons <38191182+phossil@users.noreply.github.com>
2022-12-29 09:02:22 -0500

    pkgs: shuffle files ... again

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,12 +1,89 @@
-final: prev:
-
-with final;
+{ lib
+, stdenv
+, fetchFromGitHub
+, autoconf
+, automake
+, bison
+, flex
+, xorg
+, alsa-lib
+, audiofile
+, imlib
+, gettext
+}:
 
 let
-  callPackage = prev.newScope final;
+  xsession = writeText "qvwm.desktop" ''
+    [Desktop Entry]
+    Type=Xsession
+    Name=QVWM
+    TryExec=@out@/bin/qvwm
+    Exec=@out@/bin/qvwm
+    Comment=Windows 9x lookalike window manager
+  '';
 in
-{
-  inherit callPackage;
+stdenv.mkDerivation rec {
+  pname = "qvwm";
+  version = "1.1.12";
 
-  qvwm = callPackage ./applications/window-managers/qvwm { };
+  src = fetchFromGitHub {
+    owner = "asveikau";
+    repo = "qvwm";
+    rev = "a0b181ef52deb221c8ea4219755e3279278af3fd";
+    sha256 = "Bq0N0RslBvjnpWfulWvlVmf7CewPQDMPIKC+BKAxQZA=";
+  };
+
+  #patches = [ ./extern_usleep-util_h.patch ];
+
+  nativeBuildInputs = [
+    autoconf
+    automake
+    bison
+    flex
+    gettext
+  ];
+
+  buildInputs = with xorg; [
+    libXpm
+    libSM
+    libXScrnSaver
+    libICE
+    libXext
+    libXrender
+  ] ++ [
+    alsa-lib
+    audiofile
+    imlib
+  ];
+
+  postInstall = ''
+    # install desktop session with absolute paths
+    substitute ${xsession} $out/share/xsessions/qvwm.desktop --subst-var out
+  '';
+
+  configureFlags = [
+    #"CXXFLAGS=--std=gnu++98"
+    "--enable-rmtcmd"
+    "--enable-xsmp"
+    "--enable-ss"
+    "--with-alsa=${lib.getLib alsa-lib}/lib/libasound.so"
+    "--without-esd"
+  ];
+
+  passthru.providedSessions = [ "qvwm" ];
+
+  /*
+    outputs = [
+    "out"
+    "man"
+    ];
+  */
+
+  meta = with lib; {
+    description = "'Windows Classic'-like X11 window manager";
+    homepage = "https://github.com/asveikau/qvwm";
+    maintainers = with maintainers; [ phossil ];
+    platforms = platforms.linux;
+    license = licenses.gpl2;
+  };
 }
