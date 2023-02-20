7814cfdf69a0b9967c808636c206d0854d91e8a2
Phosu Parsons <38191182+phossil@users.noreply.github.com>
2023-02-20 18:57:38 -0500

    all: simplify more

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "package of Qvwm";
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
@@ -12,10 +11,5 @@
     in
     {
       packages.${system}.default = pkgs.callPackage ./derivation.nix { };
-
-      devShells.${system}.default = pkgs.mkShell {
-        # use 'buildInputs' if using 'devEnv'
-        inputsFrom = [ self.packages.${system}.default ];
-      };
     };
 }
